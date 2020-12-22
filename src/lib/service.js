ac885420bffdb838e117de5d700f65422f20ae41
nisaelek <nisaelek@github.com>
2020-12-22 23:08:50 +0300

    initialized

---
+++ src/lib/service.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import axios from "axios";
+
+const ENDPOINT = "https://jsonplaceholder.typicode.com";
+const userid= "https://jsonplaceholder.typicode.com/users/id";
+async function getData(userid) {
+	try {
+		const { data:user  } = await axios.get(`${ENDPOINT}/users/id`);
+		console.log(users);
+	} catch (e) {
+		console.log(e);
+	}
+}
+
+export default getData();
